5ae18a5dccf5657af95681b2cbd486a182d669a6
Praful Patnecha <prafulpatnecha@gmail.com>
2024-08-24 07:27:28 +0530

    24-8-24

--- src/inharitance_For_Java.java
+++ src/inharitance_For_Java.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
+//Single inharitance
+
+class A
+{
+    public int a=10;
+    private int b=20;
+    protected  int c = 10+20;
+
+}
+class B extends A{
+    void getAValues()
+    {
+        int value = c-a;
+        System.out.println(a+" + "+ value +" = "+ c);
+    }
+}
+
+
 public class inharitance_For_Java {
-    
+    public static void main(String args[])
+    {
+        Scanner sin= new Scanner(System.in);
+        B b = new B();
+        b.getAValues();
+    }
 }
