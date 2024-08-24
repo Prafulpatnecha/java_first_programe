7eaf5d9335dbc8ebfb6f839e2394f5de4aef69c5
Praful Patnecha <prafulpatnecha@gmail.com>
2024-08-24 13:24:30 +0530

    24-8-24

--- src/inharitance_For_Java.java
+++ src/inharitance_For_Java.java
@@ -6,23 +6,43 @@ class A
 {
     public int a=10;
     private int b=20;
-    protected  int c = 10+20;
+    protected  int c = a+b;
 
 }
-class B extends A{
+
+abstract class C
+{
+    void idsp()
+    {
+        System.out.println("Hello");
+    }
+    abstract void upcomingFeature();
+}
+
+class D extends C
+{
+    @Override
+    void upcomingFeature() {
+       System.out.println("C to D");
+    }
+}
+class B extends A
+{
     void getAValues()
     {
         int value = c-a;
         System.out.println(a+" + "+ value +" = "+ c);
+
     }
 }
-
-
 public class inharitance_For_Java {
     public static void main(String args[])
     {
-        Scanner sin= new Scanner(System.in);
+        Scanner sin = new Scanner(System.in);
         B b = new B();
+        C c= new D();
+        c.upcomingFeature();
+        c.idsp();
         b.getAValues();
     }
 }
---
+++ src/interfaceInharitance.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+interface Auth{
+    void login();
+    void logout();
+}
+interface Theme{
+    void dark();
+    void light();
+}
+
+class Validation implements Theme,Auth
+{
+    String value = "Hello Java Come in flutter Grow";
+    @Override
+    public void login() {
+        System.out.println("Login");
+        System.out.println(value);
+    }
+
+    @Override
+    public void logout() {
+        System.out.println("logout");
+    }
+
+    @Override
+    public void light() {
+        System.out.println("light");
+
+    }
+
+    @Override
+    public void dark() {
+        System.out.println("dark");
+    }
+}
+
+public class interfaceInharitance
+{
+
+    public static void main(String args[])
+    {
+        Scanner scanner=new Scanner(System.in);
+        Validation validation=new Validation();
+        validation.dark();
+        validation.light();
+        validation.login();
+        validation.logout();
+        System.out.println();
+    }
+
+
+}
